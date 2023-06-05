6a6a0a3b7b85eba0051e4431764bfda8a15d9972
gourob <gourob.mazumder@enosisbd.com>
2023-06-05 22:54:37 +0600

    added font and login form

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,17 +5,48 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Colors.blue,
-          ),
-        ),
-      ),
-    );
+    return Material(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset("assets/images/login_image.jpg"),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: const [
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "john@example.com",
+                      label: Text("User Name"),
+                    ),
+                  ),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "password",
+                      label: Text("Password"),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print("Button Pressed");
+              },
+              child: const Text(
+                "Log In",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: "CeraPro",
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
